5c727e53bfbb5e12c15e8e149902b907a0bc8b1c
rustam20.04 <rust-shafik@yandex.ru>
2024-10-27 21:33:30 +0400

    First_commit_ZipCode_field

---
+++ src/test/java/ZipCodeTest.java
@@ -0,0 +1,89 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+public class ZipCodeTest {
+    private WebDriver browser;
+
+    @BeforeTest
+    public void setup(){
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--incignito");
+        options.setHeadless(true);
+        browser = new ChromeDriver(options);
+        
+    }
+
+
+    @Test
+    public void test1() {
+        browser.get("https://www.sharelane.com/");
+        browser.findElement(By.xpath("//b[text() = 'ENTER']")).click();
+        browser.findElement(By.xpath("//a[@href='./register.py']")).click();
+        WebElement zipCode = browser.findElement(By.cssSelector("input[name='zip_code']"));
+        zipCode.sendKeys("12345");
+        browser.findElement(By.cssSelector("input[value = 'Continue']")).click();
+        String expectedTitle = "ShareLane: Learn to Test";
+        String actualTitle = browser.getTitle();
+        Assert.assertEquals(actualTitle,expectedTitle);
+
+
+    }
+    @Test
+    public void testLettersInsteadOfNumbers() {
+        browser.get("https://www.sharelane.com/");
+
+        browser.findElement(By.xpath("//b[text() = 'ENTER']")).click();
+        browser.findElement(By.xpath("//a[@href='./register.py']")).click();
+        WebElement zipCode = browser.findElement(By.cssSelector("input[name='zip_code']"));
+        zipCode.click();
+        zipCode.sendKeys("Hello");
+        browser.findElement(By.cssSelector("input[value = 'Continue']")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class=error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+
+    }
+
+    @Test
+    public void testNumberWithSpace() {
+        browser.get("https://www.sharelane.com/");
+
+        browser.findElement(By.xpath("//b[text() = 'ENTER']")).click();
+        browser.findElement(By.xpath("//a[@href='./register.py']")).click();
+        WebElement zipCode = browser.findElement(By.cssSelector("input[name='zip_code']"));
+        zipCode.click();
+        zipCode.sendKeys("12 345 67");
+        browser.findElement(By.cssSelector("input[value = 'Continue']")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class=error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+
+    }
+    @Test
+    public void testSymbolsInsteadOfNumbers() {
+        browser.get("https://www.sharelane.com/");
+        browser.findElement(By.xpath("//b[text() = 'ENTER']")).click();
+        browser.findElement(By.xpath("//a[@href='./register.py']")).click();
+        WebElement zipCode = browser.findElement(By.cssSelector("input[name='zip_code']"));
+        zipCode.click();
+        zipCode.sendKeys("!@#$%^^&**(");
+        browser.findElement(By.cssSelector("input[value = 'Continue']")).click();
+        String errorMessage = browser.findElement(By.cssSelector("[class=error_message]")).getText();
+        Assert.assertEquals(errorMessage, "Oops, error on page. ZIP code should have 5 digits");
+
+    }
+
+
+
+
+    @AfterTest
+    public void closeDriver() {
+        if (browser != null){
+            browser.quit();
+        }
+    }
+}
